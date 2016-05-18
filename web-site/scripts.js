3b80b69ea6124dc832d64ca399494b29658f88ed
Cymetria <cymetria@MacBook-Pro-de-Cymetria-4.local>
2016-05-17 20:42:35 -0500

    Few things in js

---
+++ web-site/scripts.js
@@ -0,0 +1,9 @@
+var bt=document.getElementById('btMenu');
+var b2=document.getElementById("b2");
+var b3=document.getElementById("b3");
+
+
+bt.addEventListener('click', function(){
+	b2.style="order: 2";
+	b3.style="order: 1";
+});
