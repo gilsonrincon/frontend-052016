204e0bfa27c8e63bfa0c7c085da3085aa43b0190
Cymetria <cymetria@MacBook-Pro-de-Cymetria-4.local>
2016-05-19 20:39:04 -0500

    Contact form added

--- responsive-web/js/script.js
+++ responsive-web/js/script.js
@@ -1,36 +1,6 @@
-var c=0;
-var content = document.getElementById('content');
+var btMenu = $('.menu span');
+var menu=$('.menu ul');
 
-content.innerHTML='<ul id="temp">';
-
-while(c<10){
-	//content.innerHTML = content.innerHTML + '<li>Item ' + c + '</li>';
-	content.innerHTML += '<li>Item ' + c + '</li>';
-	//c = c + 1;
-	c++;
-}
-content.innerHTML += "</ul>";
-
-var temp = document.getElementById('temp');
-
-setTimeout(function(){
-	temp.innerHTML = "Fin";
-},5000);
-
-var thread = setInterval(function() {
-	temp.innerHTML += "<p>New P</p>";
-}, 1000);
-
-setTimeout(function(){
-	clearInterval(thread);
-},10000);
-
-
-
-
-
-
-
-
-
-
+btMenu.click(function(){
+	menu.slideToggle('slow');
+});
