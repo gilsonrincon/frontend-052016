52091e1f8e515d4be7b3c28e74d3511cbc281ba1
Gilson Rincon <gilson@paradisosolutions.com>
2016-06-04 23:46:23 -0500

    Class 13: v3 - Controllers

---
+++ angular-site/app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//We define the ng-app (phonecatApp)
+var phonecatApp = angular.module('phonecatApp',[]);
+
+//Define the PhoneListController
+phonecatApp.controller('PhoneListController', function PhoneListController($scope){
+	$scope.phones = [
+		{
+			name: 'Nexus S',
+      		snippet: 'Fast just got faster with Nexus S.'
+		},
+		{
+			name: 'Motorola XOOM™ with Wi-Fi',
+      		snippet: 'The Next, Next Generation tablet.'
+		},
+		{
+			name: 'MOTOROLA XOOM™',
+      		snippet: 'The Next, Next Generation tablet.'
+		}
+	];
+});
